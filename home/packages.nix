c16d58f48c69bfa40e0586f7a92794b5ee7eb417
Ziyun Li <me@ziyunli.dev>
2023-03-25 18:31:45 -0700

    add tig and rust/cargo

--- home/packages.nix
+++ home/packages.nix
@@ -3,8 +3,24 @@
 }:
 
 let
+  gitTools = with pkgs.gitAndTools;
+    [ diff-so-fancy git-codeowners gitflow ]
+    ++ (with pkgs; [
+      tig
+      difftastic
+      git-annex
+      git-crypt
+    ]);
+
   nixTools = with pkgs; [
     cachix
     lorri
   ];
-in nixTools
+
+  rustTools = with pkgs; [
+    rustup # for things like `cargo init`
+  ];
+in
+gitTools
+++ nixTools
+++ rustTools
