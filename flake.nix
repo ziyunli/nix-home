dbef9fd6429d99ce225f980d85752a28322ec87a
Ziyun Li <me@ziyunli.dev>
2023-06-02 00:12:50 -0700

    bump to 23.05

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
       # Values you should modify
       username = "ziyunli"; # $USER
       system = "aarch64-darwin";  # x86_64-linux, aarch64-multiplatform, etc.
-      stateVersion = "22.11";     # See https://nixos.org/manual/nixpkgs/stable for most recent
+      stateVersion = "23.05";     # See https://nixos.org/manual/nixpkgs/stable for most recent
 
       # System-specific Nixpkgs
       pkgs = import nixpkgs {
