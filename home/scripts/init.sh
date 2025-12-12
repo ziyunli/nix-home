090e4bbf2c3cc23a116873a4005a541924b85c9f
Ziyun Li <me@ziyunli.dev>
2025-12-11 21:26:04 -0800

    adding .local for ampcode

--- home/scripts/init.sh
+++ home/scripts/init.sh
@@ -24,6 +24,8 @@ eval "$(rbenv init - --no-rehash zsh)"
 
 source "$HOME/.rye/env"
 
+export PATH="$HOME/.local/bin:$PATH"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
